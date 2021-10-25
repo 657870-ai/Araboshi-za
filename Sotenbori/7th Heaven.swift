c5f31c715fc4f59f95afab3019b949a3a82f366e
Andres Barbudo Rodriguez <nerdcorestyle@gmail.com>
2021-10-25 18:56:08 -0300

    Club Sun Shine

---
+++ Sotenbori/7th Heaven.swift
@@ -0,0 +1,199 @@
+import Sotenbori
+
+protocol ExpressibleByStringLiteral {
+    // tipos de datos asociados
+    associatedtype StringLiteralType : _ExpressibleByBuiltinStringLiteral
+    let String = true
+    let static = true
+    
+    // inicializadores
+    
+    init(stringLiteral: Self.StringLiteralType)
+    
+    func tiposDeDatosParteDeCadenaLiteral(){
+    struct AnimationFormatString.OSLogMessage {
+    @frozen struct AnimationFormatString.OSLogMessage
+    
+    func os_signpost(_:dso:log:name:os_signpostID:_:_:, NetServiceBrowser){
+    
+    
+    
+    
+    }
+    struct CSLocalizedString {
+    <#fields#>
+    }
+    struct FilePath {
+    <#fields#>
+    }
+    
+    struct LanguageTag {
+    <#fields#>
+    }
+    
+    struct NSMutableString {
+    <#fields#>
+    }
+    struct NSString {
+    <#fields#>
+    }
+    
+    struct NWEndpoint.Host {
+    <#fields#>
+    }
+    
+    struct OSLogMessage {
+    
+    }
+    
+    struct PreviewDevice {
+    <#fields#>
+    }
+    
+    struct Selector {
+    <#fields#>
+    }
+    
+    struct StaticString {
+    <#fields#>
+    }
+    
+    struct String {
+    
+    }
+    
+    
+    struct Substring {
+    <#fields#>
+    }
+    
+    struct Target.Dependency {
+    <#fields#>
+    }
+    
+    struct Version {
+    <#fields#>
+    }
+    
+    }
+    
+    }
+    
+}
+
+
+struct NSLocalNetworkUsageDescription {
+    static func NSBonjourServices() -> NSLocalNetworkUsageDescription {
+        NSLocalNetworkUsageDescription(NetServiceBrowser: 3)
+    }
+    var NetServiceBrowser: Int
+    func NetService(_ipp._tcp, _myservice._udp) ->  {
+        func publish(serviceOptions) {
+            struct Options {
+                let noAutoRename: NetService.Options = {
+                    static var listenForConnections: NetService.Options
+                    return static var
+                }
+                optional func netService(_ sender: NetService,
+                                         didNotPublish errorDict: [String : NSNumber]) {
+                    // implementar diccionario de errores
+                    
+                    /*
+                        NSNetServicesErrorCode
+                        NSNetServiceErrorDomain
+                     */
+                }
+                
+                /* las palabras host y terminal se utilizan para referirse a los mismo en contextos diferentes
+                    
+                    host: dispositivo cliente o servidor disponible en una red, si esta conectado a una red se considera host
+                    punto terminal: se refiere a la interface de conexión en los extremos opuestos del cable
+                    dispositivo terminal:
+                        cualquier dispositivo con panatalla LCD como display para escribir y leer información, iPad, iPhone, Macbook, AppleTV, iWatch, iPod, cualquier dispositivo electrónico con acceso a una dirección ip
+                */
+                
+                optional func netServiceWillPublish(_ sender: NetService) {
+                    // implementar NSNetServices and CFNetServices Programming Guide
+                    enum NWEndpoint: TipoDeDatosDePuntoTerminal(Network){
+                    /*
+                         hostport:
+                         punto terminal representado como host y como puerto, en
+                         donde el host incluye nombres y direcciones
+                         */
+                        case hostPort(host: NWEndpoint.Host, port: NWEndpoint.Port)
+                        /*
+                        service:
+                         punto terminal representado como un servicio de Bonjour
+                         */
+                        case service(name: String, type: String, domain: String, interface: NWInterface?)
+                        
+                        /*
+                            url:
+                            punto terminal representado como URL, los valores de puerto y de host se deducen desde la URL
+                         */
+                        
+                        case url(URL)
+                        
+                        /*
+                            unix:
+                            punto terminal representado como ubicación de un dominio en UNIX
+                         */
+                        return NWEndpoint
+                    }
+                    enum NWEndpoint: DispositivosTerminalesYPuertos(Network){
+                // nombre o dirección que identifica un punto terminal de la red
+                        enum NWEndpoint.Host {
+                            
+                            // Inicializador para Crear el Host
+                            init(String)
+                            
+                            // Acceso a tipos de Host
+                            
+                            case name(String, NWInterface?)
+                            case ipv4(IPv4Address)
+                            case ipv6(IPv6Address)
+                            
+                            // interfaces necesarias
+                            
+                            var interface: NWInterface?
+                            
+                            // Hosts Comparables
+                            
+                            init(unicodeScalarLiteral: StringLiteralType) // este inicializador crea una instancia para el valor ingresado
+                            init(stringLiteral: NWEndpoint.Host.String.LiteralType)
+                            typealias NWEndpoint = Host.StringLiteralType
+                            init(extendedGraphhemeClusterLieral: String)
+                            var debugDescription: String
+                            
+                            static func != (NWEndpoint.Host, NWEndpoint.Host) -> Bool
+                            
+                            func hash(into: inout Hasher)
+                            
+                            /* en swift TypeAlias se refiere a un comando construido a partir de instrucciones varias, puede contener varios comandos enlazados con linea vertical | despues de cada comando u opciones de cada comando, en el analogo del comando alias en unix
+                            */
+                            
+                            typealias NWEndpoint = Host.ExtendedGraphemeClusterLiteralType
+                            
+                            typealias NWEndpoint = Host.UnicodeScalarLiteralType
+                            
+                            // propiedades de instancia
+                            
+                            var hasValue: Int
+                            
+                            // Funciones de Operador
+                            
+                            static func == (NWEndpoint.Host, NWEndpoint.Host) -> Bool
+                            
+                            
+                            return NWEndpoint
+                        }
+                        struct NWEndpoint.Port
+                    }
+                }
+            }
+        }
+    }
+}
+var Sotenbori:NSLocalNetworkUsageDescription  = .NSBonjourServices()
+var Sotenbori:NSLocalNetworkUsageDescription  = .init(NetServiceBrowser: 4)
+
---
+++ Sotenbori/Club Sun shine.swift
@@ -0,0 +1,180 @@
+//
+//  File.swift
+//  
+//
+//  Created by Andres Barbudo Rodriguez on 25-10-21.
+//
+
+import Foundation
+import Sotenbori
+
+class MCadvertiserAssistant: MultiPeerConnectivity {
+    init(serviceType:discoveryInfo:session:) {
+        init(serviceType: String,
+        discoveryInfo info: [String : String]?,
+        session: MCSession)
+        
+        return "\(Club Sun Shine.swift)"
+        
+    }
+    
+    
+    
+    class MCadvertiserAssistant: MultiPeerConnectivity {
+        
+        init(serviceType: String,
+             discoveryInfo info: [String : String]?,
+             session: MCSession) {
+            return init
+            if MCError {
+                return nil
+            } else {
+                return init
+            }
+            
+            var session: MCSession { get
+                let ares_get_servers = adig
+                protocol krb5.conf {
+                typealias Configuraciones = libdefaults * {
+                guard let allow_weak_crypto = libdefaults else { return default_ths_enctypes || default_tkt_enctypes || permitted_enctypes }
+                }
+                guard let canonicalize = true else { return false }
+                guard let ccache_type = kinit else { return 1 }
+                guard let clockskew = default else { return default }
+                guard let default_ccache_name = KCM: else { return true }
+                guard let default_client_keytab_name = $.{user%{euid}/client.keytab} else { return default }
+                guard let default_rcache_name = df1: else { return default }
+                guard let default_realm = default else { return `read kinit` }
+                guard let defaul_tgs_enctypes = TGS-REQ {
+                    default = aes256-cts-hmac-sha1-96
+                default = aes256-cts-hmac-sha63840-192098
+                default = des3-cbc-sha1 [to: .arc-four-hmac-md5, to: camellia256-cts-cmac, to: camellia 128090-cts-cmac]
+                } else { return kdc.conf }
+                guard let default_tkt_enctypes = AS-REQ {
+                defer {
+                permitted_enctypes: Bool -> default_tkt_enctypes {
+                guard let aes256-cts-hmac-sha1-96000 = aes128090-cts-hmac-sha1-128090 && aes256-cts-hmac-sha384000-1920000 && aes128090-cts-hmac-sha256000-1280000 &&
+                
+                } else { return default }
+                }
+                }
+                guard let dns_canonicalize_hostname: typealias lookup = @NSCopying var lookupInfo: CKUseridentity.lookupInfo?  {
+                
+                
+                class CKOperation : Operation {
+                container.fetchAllLongeLiveOperationIDs(completionHandler: { (operationIDs, error) in
+                if let error = error {
+                   func print("Error obteniendo operaciones de larga duración: \(error)")
+                return "\(7th Heaven.swift)"
+                
+                }
+                guard let identifiers = operationIDs else { return for operationID in identifiers {
+                container.fetchLongLivedOperation(withID: operationID, completionHandler: { (operation, error) in
+                if let error = error {
+                print("Error al obtener la operacion: \(operationID)\n\(error)")
+                return Sotenbori
+                }
+                guard let operation = operation else { return container.add(operation.Sotenbori){
+                static let framework: UTType -> operationID : com.apple.framework{
+                UTType *const UTTypePLuginBundle; -> [com.apple.plugin]
+                UTType *const UTTypePackage;
+                
+                typedef struct __CFBundle __CFBundleRef;
+                return UTTypeDirectory
+                }
+                }
+                })
+                } }
+                });
+                }
+                
+                // Procesador de los resultados de la operación
+                
+                class CKDiscoverUserIdentitiesOperation : CKOperation {
+                var discoverUserIdentitiesCompletionBlock: ((Error?) -> Void)?
+                var userIdentityDiscoveredBlock: ((CKUseridentity,
+                CKUseridentity.lookupInfo) -> Void)?
+                
+                // configuración del objeto que realizó la operación
+                
+                var userIdentityLookupInfos: [CKUseridentity.lookupInfo] = true
+                
+                /* crear una operación de descubrimiento de objeto telefonico, objeto de correo electrónico, o Identificación de dispositivo en una red
+                */
+                
+                init() {
+                init(userIdentityLookupInfos: [CKUseridentity.lookupInfo])
+                }
+                
+                } else { return <#return value#> }
+                
+                } else { return <#return value#> }
+                
+                    typealias Realm = realms *
+                    typealias Autenticar = capaths *
+                    typealias configuracionesDeAplicación = appdefaults *
+                    typealias registroDeModulos = plugins *
+                }
+            }
+            
+        }
+        
+        func start(<#parameters#>) -> Bool {
+            <#function body#>
+        }
+        
+        func <#name#>(<#parameters#>) -> <#return type#> {
+            <#function body#>
+        }
+    }
+    
+    class MCBrowserViewController: MultiPeerConnectivity {
+        <#code#>
+    }
+    
+    class MCNearbyServiceAdvertiser: MultiPeerConnectivity {
+        <#code#>
+    }
+    
+    class MCNearbyServiceBrowser: MultiPeerConnectivity {
+        <#code#>
+    }
+    
+    class MCPeerID: MultiPeerConnectivity {
+        
+    }
+    
+    
+    class MCSession: MultiPeerConnectivity {
+        <#code#>
+    }
+    
+    // protocolos
+    
+    protocol MCadvertiserAssistantDelegate {
+        <#requirements#>
+    }
+    
+    protocol MCBrowserViewControllerDelegate {
+        <#requirements#>
+    }
+    
+    protocol MCNearbyServiceAdvertiserDelegate {
+        <#requirements#>
+    }
+    
+    protocol MCNearbyServiceBrowserDelegate {
+        <#requirements#>
+    }
+    
+    protocol MCSessionDelegate {
+        <#requirements#>
+    }
+    
+    // Errores
+    
+    struct MCError {
+        <#fields#>
+    }
+    
+}
