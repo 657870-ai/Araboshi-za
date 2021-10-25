20dbf268c286458cf0ec1f79c3bff07a1b4da404
Andres Barbudo Rodriguez <nerdcorestyle@gmail.com>
2021-10-25 17:10:46 -0300

    7th Heaven
    Sotenbori

---
+++ Sotenbori/Sotenbori/Sotenbori.h
@@ -0,0 +1,18 @@
+//
+//  Sotenbori.h
+//  Sotenbori
+//
+//  Created by Andres Barbudo Rodriguez on 25-10-21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sotenbori.
+FOUNDATION_EXPORT double SotenboriVersionNumber;
+
+//! Project version string for Sotenbori.
+FOUNDATION_EXPORT const unsigned char SotenboriVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sotenbori/PublicHeader.h>
+
+
