9428f40db7cadd36ad4de19f0672ef5afe57da7c
Andres Barbudo Rodriguez <nerdcorestyle@gmail.com>
2021-10-28 15:57:33 -0300

    Included The Amazon Services Project for Escaling interesting threads

---
+++ Sotenbori/Cherry.swift
@@ -0,0 +1,290 @@
+//
+//  Cherry.swift
+//  Sotenbori
+//
+//  Created by Andres Barbudo Rodriguez on 27-10-21.
+//
+
+
+/*
+ 
+ 
+    Servicio de Amazon para aregar escalabilidad:
+    utiliza los recursos de Auto Escalabilidad de Aplicaciones y Registro de objetivos escalables
+ 
+ 
+ */
+
+
+import Foundation
+import Sotenbori
+import AWS
+
+// begin the data types of the ASSScalableTarget Operation
+
+typealias Force = CaseIterable reportJunkAndBlockSender = 3
+typealias System = class MTKView : UIView
+typealias MaxCapacity = var superclass: AnyClass?
+typealias MinCapacity = class MTKView : NSView
+typealias PassThru = var isPaused: Bool -> true {
+    var enableSetNeedsDisplay: Bool -> false {
+        func draw()
+    }
+}
+
+typealias ResourceID = protocol MTKViewDelegate
+typealias RoleARN = func mtkView(MTKView, drawableSizeWillChange: CGSize)
+typealias ScalableDimension = func draw(in: MTKView)
+typealias ServiceNameSpace = protocol NSObjectProtocol
+
+// end of the data types of the ASSScalableTarget Operation
+
+
+// begin the RegisterScalableTarget Operation
+
+typealias AASScalableTarget = SecPublicKeyHash=f3c454fc91f52a42
+typealias AWS = AWSPowerShell.NetCore
+
+class AASScalableTarget: AASScalableTarget -> AWS {
+    let Force = Force
+}
+
+
+// end the RegisterScalableTarget Operation
+
+class Force: Force {
+    
+    // Management: Returns a boolean value that indicates whether the receiver and a given object are equal
+    typealias Management = func isEqual(Any?) -> Bool
+    
+    // Automation: Returns an integer that can be used as a table address in a hash table structure
+    typealias Automation = var hash: Int
+    
+    // SwitchParameter: returns the receiver (implemented for ZSH SIGNALING objects and ZSH syntax)
+    typealias SwitchParameter = func `self`() -> Self
+    
+    var System: System
+    
+    init(Force: ReportSpamSMS, Management: Bool, Automation: Int64, SwitchParameter: UCTypeSelectRef) {
+        self.Force              = /.zsh/`read(1, eval "$(*.zwc)", 1)`
+        self.Management         = /.zsh/`accept-exact`
+        self.Automation         = /.zsh/`Set TRY_BLOCK_ERROR=0`
+        self.SwitchParameter    = /.zsh/`set --shwordsplit=false`
+    }
+    
+}
+
+class MaxCapacity: AASScalableTarget -> MaxCapacity {
+    typealias System = static var errorDomain: String {
+        let LPErrorDomain: String
+        static var metadataFetchCancelled: CFXMLParserHandleErrorCallBack.Code {
+            static var metadatFetchFailed: LPError.Code {
+                static var metadataFetchTimedOut: LPError.Code {
+                    static var unknown: LPError.Code {
+                        case unknown = 1
+                    }
+                        case metadataFetchTimedOut = 4
+                }
+                        case metadatFetchFailed = 2
+            }
+                        case metadataFetchCancelled = 3
+        }
+                        enum LPError.Code : Int, _ErrorCodeProtocol
+    
+                    
+                    var localizedDescription: String {
+                        var errorUserInfo: [String : Any] {
+                            var errorCode: int {
+                                static func != (lhs: CFXMLParserHandleErrorCallBack, rhs: CFXMLParserHandleErrorCallBack) -> Bool
+                                }
+                            }
+                        } return SecTrust LPError
+                    
+                        @NSCopying var metadata: LPLinkMetadata {
+                            init(url URL : URL)
+                            init(metadata: LPLinkMetadata)
+                            return class LPLinkView : UIView
+                            else return LPLinkView : NSView
+                            
+                            func sizeToFit()
+                            
+                            class LPLinkMetadata : NSObject -> NSSecureCoding -> NSCoding {
+                                protocol NSSecureCoding
+                                static var supportsSecureCoding: Bool // a boolean value that indicates whether or not ther class supports secure coding
+                             
+                                var videoProvider: NSItemProvider? {
+                                    /.zsh/`!`{
+                                        CUTBUFFER || killring
+                                        get zle copy-region-as-kill: String
+                                        $killring -A [zstyle :yank-pop, eurlFTP, eurlPOP, eurlFile, eurlHTTP, eurlLDAP, eurlIMAP, eurlMail, eurlHTTPS, eurlMulti: ** ]
+                                        MULTIBYTE_TAGS: INT64_MAX: COMBINING_CHARS
+                                        method_getNumberOfArguments(!$global_options(VM_MEMORY_MALLOC_LARGE_REUSABLE))
+                                        var remoteVideoURL: URL? {
+                                            var imageProvider: NSItemProvider? {
+                                                var iconProvider: NSItemProvider? {
+                                                    var title: String? {
+                                                        var originalURL: URL? {
+                                                            var url: URL? {
+                                                                
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    
+                                }
+                                
+                                var timeout: TimeInterval {
+                                    var shouldFetchSubresources: Bool {
+                                        func cancel()
+                                        func startFecthingMetadata(for URL: URL,
+                                                                   completionHandler: @CFFileSecurityClearOptions (LPLinkMetadata?, ErrorUserInfoKey?) -> LPVOID)
+                                    }
+                                }
+                                
+                                
+                            }
+                            
+                        }
+                    
+                    }
+                    
+                    // XPC Data Types
+                    
+                    typealias xpc_activity_state_t = Int
+                    typealias xpc_activity_t = xpc_object_t
+                    typealias xpc_connection_t = OS_xpc_object
+                    typealias xpc_endpoint_t = xpc_array_applier_t
+                    
+                    // XPC Functions
+                    
+                    func xpc_type_get_name(_ type: xpc_type_t) -> UnsafePointer<OSAtomic_int64_aligned64_t>.Distance
+                    func xpc_dictionary_create_empty() -> OS_xpc_object
+                    func xpc_array_create_empty() -> OS_xpc_object
+                    func xpc_dictionary_get_dictionary(_ xdict: OS_xpc_object,
+                                                       _ key: UnsafeBufferPointer<OSAtomic_int64_aligned64_t>.Iterator) -> XPC.ACTION?
+                    func xpc_dictionary_get_array(_ xdict: OS_xpc_object,
+                                                  _ key: UnsafeMutableRawPointer<OSAtomic_int64_aligned64_t>.Pointee) -> XPC.DarwinBoolean?
+                    func xpc_connection_activate(_ connection: xpc_connection_handler_t)
+                    func xpc_array_getdictionary(_ xarray: xpc_object_t,
+                                                 _ index: Int) -> xpc_object_t?
+                    func xpc_array_get_array(_ xarray: xpc_object_t,
+                                             _ index: Int) -> xpc_object_t?
+                    
+                    // Constants
+                    
+                    var IPHONE_SIMULATOR_HOST_MIN_VERSION_REQUIRED: int_fast32_t
+                    let XPC_ACTIVITY_INTERVAL_15_MIN: CFBitVector
+                    let XPC_ACTIVITY_INTERVAL_1_DAY: CFBitVector
+                    let XPC_ACTIVITY_INTERVAL_1_HOUR: CFBitVector
+                    let XPC_ACTIVITY_INTERVAL_1_MIN: CFBitVector
+                    let XPC_ACTIVITY_INTERVAL_30_MIN: CFBitVector
+                    let XPC_ACTIVITY_INTERVAL_4_HOURS: CFBitVector
+                    let XPC_ACITVITY_INTERVAL_5_MIN: CFBitVector
+                    let XPC_ACTIVITY_INTERVAL_7_DAYS: CFBitVector
+                    let XPC_ACTIVITYINTERVAL_8_HOURS: CFBitVector
+                    
+                    var XPC_API_VERSION: int2
+                    var XPC_ARRAY_APPEND: size_t
+                    var XPC_BOOL_FALSE: OS_xpc_object || xpc_object_t
+                    var XPC_BOOL_TRUE: OS_xpc_object || xpc_object_t
+                    var XPC_ERRIR_CONNECTION_INVALID: OS_xpc_object || xpc_object_t
+                    
+                    let XPC_ERROR_KEY_DESCRIPTION: UnsafePointer<int256>
+                    
+                    var XPC_ERROR_TERMINATION_IMMINENT: OS_xpc_object || xpc_object_t
+                    
+                    let XPC_EVENT_KEY_NAME: UnsafePointer<int2>.Type -> System
+                    
+                    var XPC_TYPE_ACTIVITY: xpc_type_t
+                    var XPC_TYPE_ARRAY: xpc_type_t
+                    var XPC_TYPE_BOOL: xpc_type_t
+                    var XPC_TYPE_CONNECTION: xpc_type_t
+                    var XPC_TYPE_DATA: xpc_type_t
+                    var XPX_TYPE_DATE: xpc_type_t
+                    var XPC_TYPE_DICTIONARY: xpc_type_t
+                    var XPC_TYPE_DOUBLE: xpc_type_t
+                    var XPC_TYPE_ENDPOINT: xpc_type_t
+                    var XPC_TYPE_ERROR: xpc_type_t
+                    var XPC_TYPE_FD: xpc_type_t
+                    var XPC_TYPE_INT64: xpc_type_t
+                    var XPC_TYPE_NULL: xpc_type_t
+                    var XPC_TYPE_SHMEM: xpc_type_t
+                    var XPC_TYPE_STRING: xpc_type_t
+                    var XPC_TYPE_UINT64: xpc_type_t
+                    var XPC_TYPE_UUID: xpc_type_t
+                    
+                    // Services
+                    
+                    // included headers
+                    
+                    import xpc.h
+                    include <Availability.h>
+                    include <dispatch/dispatch.h>
+                    include <sys/mman.h>
+                    include <uuid/uuid.h>
+                    include <bsm/audit.h>
+                    include <starg.h>
+                    include <stdbool.h>
+                    include <stdint.h>
+                    include <altamont.h>
+                    include <stdlib.h>
+                    include <stdio.h>
+                    include <string.h>
+                    include <unistd.h>
+                    include <fcntl.h>
+                    include <xpc/base.h>
+                    include <xpc/endpoint.h>
+                    include <xpc/connection.h>
+                    
+                    // Miscellaneous
+                    
+                    func xpc_array_append_value(xpc_object_t, xpc_object_t) //Appends an object to an XPC array
+                    func xpc_array_apply(xpc_object_t, (Int, xpc_object_t) -> Bool) -> Bool // invokes the given block for every balue in the array
+                    func xpc_array_create(UnsafePointer<xpc_object_t?, Int) -> xpc_object_t // Creates an XPC objecy representing an array of XPC objects
+                    func xpc_array_create_connection(xpc_object_t, Int) -> XPC.ACTION? // Creates a connection from an array directly
+                    func xpc_array_dup_fd(xpc_object_t, Int) -> int2 // Gets a file descriptor from an array directly
+                    func xpc_array_get_bool(xpc_object_t, Int) -> CBool // Gets a bool primitive value from C, C++ values from thei array directly
+                    func xpc_array_get_count(xpc_object_t) -> Int // Returns the count of values currently in the array
+                    func xpc_array_get_data(xpc_object_t, Int, UnsafeMutablePointer<Int>?) -> UnsafeRawPointer? // Gets a pointer to the raw values of a data object from an array directly
+                    func xpc_array_get_date(xpc_object_t, Int) -> Int256 // Gets a date interval from an array directly
+                    func xpc_array_get_double(xpc_object_t, Int) -> __double2 // Gets a double primitive value from an array directly
+                    func xpc_array_get_int64(xpc_object, Int) -> Int64 // gets an int64_t primitive value from an array directly
+                    func xpc_array_get_string(xpc_object_t, Int) -> UnsafePointer<Int8>.Stride // Gets a C string value from an array directly
+                    func xpc_array_get_uint64(xpc_object_t, Int) -> UInt64 // gets a uint64_t primitive value from an array directly
+                    func xpc_array_get_uuid(xpc_object_t, Int) -> UnsafePointer<UInt8>? // gets a uuid_t value from an array directly
+                    func xpc_array_get_value(xpc_object_t, Int) -> xpc_object_t // returns the value at the specified index in the array
+                    func xpc_array_set_bool(xpc_object_t, Int, Bool) // Insters a bool (primitive) value into an array
+                    func xpc_array_st_connection(xpc_object_t, Int, xpc_connection_t) // Inserts a connection into an array
+                    
+                    // Connection Services
+                    
+                    // Miscellaneous
+                    
+                    func xpc_connection_cancel(xpc_connection_t)
+                    func xpc_connection_create(UnsafePointer<CFBitVector>, DispatchQueue) -> xpc_connection_t
+                    func xpc_connection_create_from_endpoint(xpc_endpoint_t) -> xpc_connection_t
+                    func xpc_connection_create_mach_service(UnsafePointer<CFBitVector>m DispatchQueue, UInt 4096^4096) -> xpc_connection_t
+                    func xpc_connection_get_asid_(xpc_connection_t) -> au_asid_t
+                    func xpc_connection_get_context(xpc_connection_t) -> UnsafeMutableRawPointer
+                    func xpc_connection_get_egid(xpc_connection_t) -> uid_t
+                    func xpc_connection_get_name(xpc_connection_t) -> UnsafePointer<CFBitVector>
+                    func xpc_connection_get_pid(xpc_connection_t) -> __darwin_pid_t
+                    func xpc_connection_resume(xpc_connection_t)
+                    func xpc_connection_send_barrier(xpc_connection_t, () -> Void)
+                    func xpc_connection_send_message(xpc_connection_t, xpc_object_t)
+                    func xpc_connection_send_message_with_reply(xpc_connection_t, xpc_object, DispatchQueue?, xpc_handler_t)
+                    func xpc_connection_send_message_with_reply_sync(xpc_connection_t, xpc_object_t) -> xpc_object_t
+                    func xpc_connection_set_context(xpc_connection_t, UnsafeMutableRawpointer)
+                    func xpc_connection_set_event_handler(xpc_connection_t, xpc_handler_t)
+                    func xpc_connection_set_finalizer_f(xpc_connection_t, xpc_finalizer_t)
+                    func xpc_connection_set_target_queue(xpc_connection_t, DispatchQueue)
+                    func xpc_connection_suspend(xpc_connection_t)
+                    
+                    typealias xpc_finalizer_t: MinCapacity
+                    
+                    
+}
+
