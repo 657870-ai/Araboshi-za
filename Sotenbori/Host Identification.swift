d4c9dea1418a77275f415ee282c78b0cdd7ffea5
Andres Barbudo Rodriguez <nerdcorestyle@gmail.com>
2021-10-29 22:30:12 -0300

    iOS Forensics - Save RAW files in the Evidence Folder

---
+++ Sotenbori/Host Identification.swift
@@ -0,0 +1,15 @@
+//
+//  Host Identification.swift
+//  Sotenbori
+//
+//  Created by Andres Barbudo Rodriguez on 29-10-21.
+//
+
+import Foundation
+
+func SecCodeCopyGuestWithAttributes(_ host: SecCode?,
+                                    _ attributes: CFDictionary?,
+                                    _ flags: SecCSFlags,
+                                    _ guest: UnsafeMutablePointer<SecCode?>) -> OSStatus {
+    
+}
